f3504cfd974c5041ace9584f5d1a02fff8d6b3f3
ForgeAndFury <forgeandfury@proton.me>
2026-02-10 22:38:37 +0000

    Add basic hello world test

--- Backend/Source/Main.cxx
+++ Backend/Source/Main.cxx
@@ -1,5 +1,19 @@
-#include <fmt/format.h>
+#include <drogon/drogon.h>
 
 int main() {
-    fmt::print("Hello, {}", "World");
+    // handler for the root path "/"
+    drogon::app().registerHandler("/", [](const drogon::HttpRequestPtr& req, 
+                                          std::function<void (const drogon::HttpResponsePtr &)> &&callback) {
+        auto resp = drogon::HttpResponse::newHttpResponse();
+        resp->setBody("<h1>Hello World from Dashboard!</h1>");
+        callback(resp);
+    });
+
+    drogon::app().addListener("0.0.0.0", 8080);
+
+    // Running the server
+    LOG_INFO << "Server running on http://localhost:8080";
+    drogon::app().run();
+
+    return 0;
 }
